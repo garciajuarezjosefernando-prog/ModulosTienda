da95ca912e0c1c5ddf5cf585b5dc64425058e111
Jose Garcia <garciajuarezjosefernando@gmail.com>
2026-05-14 01:31:45 -0600

    pagina Principal

--- obj/Debug/net10.0/ModulosTienda.AssemblyInfo.cs
+++ obj/Debug/net10.0/ModulosTienda.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModulosTienda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c9633e828d5f40b4154d0828f806a928a27a3a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModulosTienda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModulosTienda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
