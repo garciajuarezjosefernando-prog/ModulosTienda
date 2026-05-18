e363671f3fb0edc1345ad0dacf82c7c0345f1313
Victor  Alfonso Berny Lazcano <vabernylazcano@gmail.com>
2026-05-18 16:53:05 -0600

    Cambios de usuarios para guardar y regresar a pantalla

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ModulosTienda.Models;
+using MySql.Data.MySqlClient;
 
 namespace ModulosTienda.Controllers
 {
     public class UsuariosController : Controller
     {
+
         public IActionResult Usuarios()
         {
             return View();
@@ -13,5 +16,68 @@ namespace ModulosTienda.Controllers
         {
             return View();
         }
+
+        private readonly IConfiguration _configuration;
+
+        public UsuariosController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [HttpPost]
+        public IActionResult Guardar(Usuario model)
+        {
+            string conexion = _configuration.GetConnectionString("MySqlConnection");
+
+            using (MySqlConnection conn = new MySqlConnection(conexion))
+            {
+                conn.Open();
+
+                
+                string queryValidar = @"SELECT COUNT(*) 
+                                    FROM Usuarios 
+                                    WHERE usuario = @usuario 
+                                    OR correo = @correo";
+
+                using (MySqlCommand cmdValidar = new MySqlCommand(queryValidar, conn))
+                {
+                    cmdValidar.Parameters.AddWithValue("@usuario", model.usuario);
+                    cmdValidar.Parameters.AddWithValue("@correo", model.correo);
+
+                    int existe = Convert.ToInt32(cmdValidar.ExecuteScalar());
+
+                    if (existe > 0)
+                    {
+                        
+                        TempData["Error"] = "El usuario o correo ya existe.";
+                        return View("NuevoUsuario");
+                    }
+                }
+
+                
+                string queryInsert = @"INSERT INTO Usuarios
+                                (usuario, nombre, contrasena, correo, tipo, activo)
+                                VALUES
+                                (@usuario, @nombre, @contrasena, @correo, @tipo, @activo)";
+
+                using (MySqlCommand cmdInsert = new MySqlCommand(queryInsert, conn))
+                {
+                    cmdInsert.Parameters.AddWithValue("@usuario", model.usuario);
+                    cmdInsert.Parameters.AddWithValue("@nombre", model.nombre);
+                    cmdInsert.Parameters.AddWithValue("@contrasena", model.contrasena);
+                    cmdInsert.Parameters.AddWithValue("@correo", model.correo);
+                    cmdInsert.Parameters.AddWithValue("@tipo", model.tipo);
+                    cmdInsert.Parameters.AddWithValue("@activo", 1);
+
+                    cmdInsert.ExecuteNonQuery();
+                }
+            }
+
+            
+            TempData["PedidoGuardado"] = true;
+            return RedirectToAction("Usuarios", "Usuarios");
+        }
+
     }
 }
---
+++ Models/ModelosModulosTienda.cs
@@ -0,0 +1,16 @@
+﻿namespace ModulosTienda.Models
+{
+    public class ModelosModulosTienda
+    {
+    }
+
+    public class Usuario
+    {
+        public string usuario { get; set; }
+        public string nombre { get; set; }
+        public string contrasena { get; set; }
+        public string correo { get; set; }
+        public string tipo { get; set; }
+        public bool activo { get; set; }
+    }
+}
